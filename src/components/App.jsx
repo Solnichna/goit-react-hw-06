8d90e428cfa1d58a0f0a3ab688fd2dde961a0a2b
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-20 21:14:08 +0200

    Part3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import store, { persistor } from '../redux/store';
+import store, { persistor } from '../redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 import ContactForm from './contactForm/ContactForm';
 import ContactList from './contactList/ContactList';
