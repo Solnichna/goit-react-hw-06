18b493375b62f01da23c18ea2b3a10a6910fadae
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-04-21 10:53:14 +0200

    Contact

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from 'react-redux';
 import Contact from '../contact/Contact';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = ({ onDeleteContact }) => {
+ 
+  const contacts = useSelector(state => state.contacts);
+
   return (
     <ul className="contact-list">
       {contacts.map(contact => (
